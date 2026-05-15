39a7625fce4cae2d91459a6cecc184ecafbced10
Titor Oopart <johnpiterzon@gmail.com>
2026-05-15 06:50:56 -0400

    feat: CRUD api implemented.

---
+++ src/app/api/places/[id]/route.js
@@ -0,0 +1,72 @@
+import pool from '@/lib/db';
+
+export async function GET(request, { params }) {
+  const { id } = await params;
+  const result = await pool.query(
+    'SELECT * FROM places WHERE id = $1',
+    [id]
+  );
+
+  if (result.rows.length === 0) {
+    return Response.json(
+      { message: `Place not found ${id}` },
+      { status: 404 }
+    );
+  }
+
+  return Response.json(result.rows[0]);
+}
+
+export async function DELETE(request, { params }) {
+  try {
+
+    const { id } = await params;
+    const result = await pool.query(
+      `DELETE FROM places
+    WHERE id = $1
+    RETURNING *`, [id]
+    )
+    return Response.json(result.rows[0])
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+}
+
+
+export async function PUT(request, { params }) {
+  try {
+    const { id } = await params;
+    const body = await request.json();
+    const allowedField = [
+      "name", "description", "location", "image_url"
+    ]
+    const bodyArray = Object.keys(body);
+    for (let i = 0; i < bodyArray.length; i++) {
+      let userBody = bodyArray[i]
+      if (!allowedField.includes(userBody)) {
+        return Response.json({ error: "Invalid body data." }, { status: 404 })
+      }
+    }
+    const { name, description, location, image_url } = body;
+    const result = await pool.query(
+      `UPDATE places
+      SET name = COALESCE($1, name),
+      description = COALESCE($2, description),
+      location = COALESCE($3, location),
+      image_url = COALESCE($4, image_url)
+      WHERE id = $5
+      RETURNING *;
+      `, [name, description, location, image_url, id]
+    )
+    if (result.rows.length === 0) {
+      return Response.json(
+        { error: "Place no encontrado" },
+        { status: 404 }
+      );
+    }
+    return Response.json(result.rows[0])
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 })
+  }
+}
+
--- src/app/api/places/route.js
+++ src/app/api/places/route.js
@@ -1,4 +1,5 @@
 import pool from '@/lib/db';
+import { Ojuju } from 'next/font/google';
 
 
 export async function GET() {
@@ -12,3 +13,39 @@ export async function GET() {
     return Response.json({ error: 'Database error' }, { status: 500 })
   }
 }
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { name, description, location, image_url } = body;
+    const allowedData = [
+      'name', 'description', 'location', 'image_url'
+    ]
+
+    const invalidField = Object.keys(body).filter(
+      (element) => !allowedData.includes(element)
+    );
+
+    if (invalidField.length > 0) {
+      return Response.json({ error: "Bad request" }, { status: 400 })
+    }
+
+    const result = await pool.query(
+      `INSERT INTO places(
+      name,
+      description,
+      location,
+      image_url
+      )
+      VALUES(
+      $1,$2,$3,$4
+      )
+      RETURNING *
+      `, [name, description, location, image_url]
+    )
+    return Response.json(result.rows[0], { status: 201 })
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 })
+  }
+}
+
